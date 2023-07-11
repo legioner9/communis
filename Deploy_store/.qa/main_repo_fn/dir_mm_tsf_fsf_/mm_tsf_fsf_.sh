4a2bb9aa5e2da67ee0e9315cafaf9d7fe117c07d
[alt]legioner9 <legioner9@inbox.ru>
2023-07-11 15:59:29 +0700

    <> _tsf_sfs_ #2

--- Deploy_store/.qa/main_repo_fn/dir_mm_tsf_fsf_/mm_tsf_fsf_.sh
+++ Deploy_store/.qa/main_repo_fn/dir_mm_tsf_fsf_/mm_tsf_fsf_.sh
@@ -254,156 +254,199 @@ EXAMP:${NORMAL}"
     # res+=("bbbbbbbbb")
     # arr+=("aaaaaaaaa")
     # res+=("bbbbbbbbb")
-
+    
     if [ $(num_01 $1) -eq 1 ] && [ $1 -le ${#arr[@]} ]; then
-
         num_res=$(($1 - 1))
-        eval \${res[${num_res}]}
+        ${res[${num_res}]}
         return 0
-
     fi
 
+    # PS3="eligendi actiones: "
+
+    # select item in "${arr[@]}"; do
+
+    #     dilectus=$item
+    #     break
+
+    # done
+
     PS3="eligendi actiones: "
-
     select item in "${arr[@]}"; do
-
-        dilectus=$item
-        break
-
+        for ((i = 0; i < 1000; i++)); do
+            if [[ ${item} == ${arr[$i]} ]]; then
+                ${res[$i]}
+                ${FNN} $@
+            fi
+            if [[ ${item} == ${arr[0]} ]]; then
+                ${res[$i]}
+            fi
+        done
     done
+    # case $dilectus in
 
-    case $dilectus in
+    # "${arr[0]}")
+    #     echo -e "${CYAN}--- ${res[0]} ---${NORMAL}" #started functions
+    #     ${res[0]}
 
-    "${arr[0]}")
-        echo -e "${CYAN}--- ${res[0]} ---${NORMAL}" #started functions
-        ${res[0]}
+    #     ;;
+    # "${arr[1]}")
+    #     echo -e "${CYAN}--- ${res[1]} ---${NORMAL}" #started functions
+    #     ${res[1]}
+    #     mm_tsf_fsf_ "$@"
+    #     ;;
 
-        ;;
-    "${arr[1]}")
-        echo -e "${CYAN}--- ${res[1]} ---${NORMAL}" #started functions
-        ${res[1]}
-        mm_tsf_fsf_ "$@"
-        ;;
+    # "${arr[2]}")
+    #     echo -e "${CYAN}--- ${res[2]} ---${NORMAL}" #started functions
+    #     ${res[2]}
+    #     mm_tsf_fsf_ "$@"
+    #     ;;
 
-    "${arr[2]}")
-        echo -e "${CYAN}--- ${res[2]} ---${NORMAL}" #started functions
-        ${res[2]}
-        mm_tsf_fsf_ "$@"
-        ;;
+    # "${arr[3]}")
+    #     echo -e "${CYAN}--- ${res[3]} ---${NORMAL}" #started functions
+    #     ${res[3]}
+    #     mm_tsf_fsf_ "$@"
+    #     ;;
 
-    "${arr[3]}")
-        echo -e "${CYAN}--- ${res[3]} ---${NORMAL}" #started functions
-        ${res[3]}
-        mm_tsf_fsf_ "$@"
-        ;;
+    # "${arr[4]}")
+    #     echo -e "${CYAN}--- ${res[4]} ---${NORMAL}" #started functions
+    #     ${res[4]}
+    #     mm_tsf_fsf_ "$@"
+    #     ;;
 
-    "${arr[4]}")
-        echo -e "${CYAN}--- ${res[4]} ---${NORMAL}" #started functions
-        ${res[4]}
-        mm_tsf_fsf_ "$@"
-        ;;
+    # "${arr[5]}")
+    #     echo -e "${CYAN}--- ${res[5]} ---${NORMAL}" #started functions
+    #     ${res[5]}
+    #     mm_tsf_fsf_ "$@"
+    #     ;;
 
-    "${arr[5]}")
-        echo -e "${CYAN}--- ${res[5]} ---${NORMAL}" #started functions
-        ${res[5]}
-        mm_tsf_fsf_ "$@"
-        ;;
+    # "${arr[6]}")
+    #     echo -e "${CYAN}--- ${res[6]} ---${NORMAL}" #started functions
+    #     ${res[6]}
+    #     mm_tsf_fsf_ "$@"
+    #     ;;
 
-    "${arr[6]}")
-        echo -e "${CYAN}--- ${res[6]} ---${NORMAL}" #started functions
-        ${res[6]}
-        mm_tsf_fsf_ "$@"
-        ;;
+    # "${arr[7]}")
+    #     echo -e "${CYAN}--- ${res[7]} ---${NORMAL}" #started functions
+    #     ${res[7]}
+    #     mm_tsf_fsf_ "$@"
+    #     ;;
 
-    "${arr[7]}")
-        echo -e "${CYAN}--- ${res[7]} ---${NORMAL}" #started functions
-        ${res[7]}
-        mm_tsf_fsf_ "$@"
-        ;;
+    # "${arr[8]}")
+    #     echo -e "${CYAN}--- ${res[8]} ---${NORMAL}" #started functions
+    #     ${res[8]}
+    #     mm_tsf_fsf_ "$@"
+    #     ;;
 
-    "${arr[8]}")
-        echo -e "${CYAN}--- ${res[8]} ---${NORMAL}" #started functions
-        ${res[8]}
-        mm_tsf_fsf_ "$@"
-        ;;
+    # "${arr[9]}")
+    #     echo -e "${CYAN}--- ${res[9]} ---${NORMAL}" #started functions
+    #     ${res[9]}
+    #     mm_tsf_fsf_ "$@"
+    #     ;;
 
-    "${arr[9]}")
-        echo -e "${CYAN}--- ${res[9]} ---${NORMAL}" #started functions
-        ${res[9]}
-        mm_tsf_fsf_ "$@"
-        ;;
+    # "${arr[10]}")
+    #     echo -e "${CYAN}--- ${res[10]} ---${NORMAL}" #started functions
+    #     ${res[10]}
+    #     mm_tsf_fsf_ "$@"
 
-    "${arr[10]}")
-        echo -e "${CYAN}--- ${res[10]} ---${NORMAL}" #started functions
-        ${res[10]}
-        mm_tsf_fsf_ "$@"
+    #     ;;
+    # "${arr[11]}")
+    #     echo -e "${CYAN}--- ${res[11]} ---${NORMAL}" #started functions
+    #     ${res[11]}
+    #     mm_tsf_fsf_ "$@"
+    #     ;;
 
-        ;;
-    "${arr[11]}")
-        echo -e "${CYAN}--- ${res[11]} ---${NORMAL}" #started functions
-        ${res[11]}
-        mm_tsf_fsf_ "$@"
-        ;;
+    # "${arr[12]}")
+    #     echo -e "${CYAN}--- ${res[12]} ---${NORMAL}" #started functions
+    #     ${res[12]}
+    #     mm_tsf_fsf_ "$@"
+    #     ;;
 
-    "${arr[12]}")
-        echo -e "${CYAN}--- ${res[12]} ---${NORMAL}" #started functions
-        ${res[12]}
-        mm_tsf_fsf_ "$@"
-        ;;
+    # "${arr[13]}")
+    #     echo -e "${CYAN}--- ${res[13]} ---${NORMAL}" #started functions
+    #     ${res[13]}
+    #     mm_tsf_fsf_ "$@"
+    #     ;;
 
-    "${arr[13]}")
-        echo -e "${CYAN}--- ${res[13]} ---${NORMAL}" #started functions
-        ${res[13]}
-        mm_tsf_fsf_ "$@"
-        ;;
+    # "${arr[14]}")
+    #     echo -e "${CYAN}--- ${res[14]} ---${NORMAL}" #started functions
+    #     ${res[14]}
+    #     mm_tsf_fsf_ "$@"
+    #     ;;
 
-    "${arr[14]}")
-        echo -e "${CYAN}--- ${res[14]} ---${NORMAL}" #started functions
-        ${res[14]}
-        mm_tsf_fsf_ "$@"
-        ;;
+    # "${arr[15]}")
+    #     echo -e "${CYAN}--- ${res[15]} ---${NORMAL}" #started functions
+    #     ${res[15]}
+    #     mm_tsf_fsf_ "$@"
+    #     ;;
 
-    "${arr[15]}")
-        echo -e "${CYAN}--- ${res[15]} ---${NORMAL}" #started functions
-        ${res[15]}
-        mm_tsf_fsf_ "$@"
-        ;;
+    # "${arr[16]}")
+    #     echo -e "${CYAN}--- ${res[16]} ---${NORMAL}" #started functions
+    #     ${res[16]}
+    #     mm_tsf_fsf_ "$@"
+    #     ;;
 
-    "${arr[16]}")
-        echo -e "${CYAN}--- ${res[16]} ---${NORMAL}" #started functions
-        ${res[16]}
-        mm_tsf_fsf_ "$@"
-        ;;
+    # "${arr[17]}")
+    #     echo -e "${CYAN}--- ${res[17]} ---${NORMAL}" #started functions
+    #     ${res[17]}
+    #     mm_tsf_fsf_ "$@"
+    #     ;;
 
-    "${arr[17]}")
-        echo -e "${CYAN}--- ${res[17]} ---${NORMAL}" #started functions
-        ${res[17]}
-        mm_tsf_fsf_ "$@"
-        ;;
+    # "${arr[18]}")
+    #     echo -e "${CYAN}--- ${res[18]} ---${NORMAL}" #started functions
+    #     ${res[18]}
+    #     mm_tsf_fsf_ "$@"
+    #     ;;
 
-    "${arr[18]}")
-        echo -e "${CYAN}--- ${res[18]} ---${NORMAL}" #started functions
-        ${res[18]}
-        mm_tsf_fsf_ "$@"
-        ;;
+    # "${arr[19]}")
+    #     echo -e "${CYAN}--- ${res[19]} ---${NORMAL}" #started functions
+    #     ${res[19]}
+    #     mm_tsf_fsf_ "$@"
+    #     ;;
 
-    "${arr[19]}")
-        echo -e "${CYAN}--- ${res[19]} ---${NORMAL}" #started functions
-        ${res[19]}
-        mm_tsf_fsf_ "$@"
-        ;;
+    # "${arr[20]}")
+    #     echo -e "${CYAN}--- ${res[20]} ---${NORMAL}" #started functions
+    #     ${res[20]}
+    #     mm_tsf_fsf_ "$@"
 
-    "${arr[20]}")
-        echo -e "${CYAN}--- ${res[20]} ---${NORMAL}" #started functions
-        ${res[20]}
-        mm_tsf_fsf_ "$@"
+    #     ;;
 
-        ;;
+    # *)
+    #     echo -e "${BLUE}---error dilectus ---${NORMAL}" #sistem info mesage
+    #     ;;
+    # esac
 
-    *)
-        echo -e "${BLUE}---error dilectus ---${NORMAL}" #sistem info mesage
-        ;;
-    esac
+    # echo -e "${BLUE}--- exec ${FNN} (num_menu) ---${NORMAL}" #started functions
+    # local arr=()
+    # local res=()
+    # arr+=("leave menu")
+    # res+=("echo bye)))")
+    # #{arr_res}
+    # # arr+=("aaaaaaaaa")
+    # # res+=("bbbbbbbbb")
+    # # arr+=("aaaaaaaaa")
+    # # res+=("bbbbbbbbb")
+    # # arr+=("aaaaaaaaa")
+    # # res+=("bbbbbbbbb")
+    # # arr+=("aaaaaaaaa")
+    # # res+=("bbbbbbbbb")
+    # if [ $(num_01 $1) -eq 1 ] && [ $1 -le ${#arr[@]} ]; then
+    #     num_res=$(($1 - 1))
+    #     ${res[${num_res}]}
+    #     return 0
+    # fi
+    # PS3="eligendi actiones: "
+    # select item in "${arr[@]}"; do
+    #     for ((i = 0; i < 1000; i++)); do
+    #         if [[ ${item} == ${arr[$i]} ]]; then
+    #             ${res[$i]}
+    #             ${FNN} $@
+    #         fi
+    #         if [[ ${item} == ${arr[0]} ]]; then
+    #             ${res[$i]}
+    #         fi
+    #     done
+    # done
+    #{zzz20}
 
     # ------------------------------------------
     # -----------------------------------------------------------------------------------------
