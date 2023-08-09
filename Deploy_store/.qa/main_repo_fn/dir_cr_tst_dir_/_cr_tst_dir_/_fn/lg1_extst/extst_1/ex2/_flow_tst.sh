201d4b0d8cb7a0ce47a3ab26811fd8c5407bd49d
[alt]legioner9 <legioner9@inbox.ru>
2023-08-09 22:54:36 +0700

    <> ln1_ ? #2

---
+++ Deploy_store/.qa/main_repo_fn/dir_cr_tst_dir_/_cr_tst_dir_/_fn/lg1_extst/extst_1/ex2/_flow_tst.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+filename=${PATH_CURR_REPO}/communis/Deploy_store/.qa/main_repo_fn/dir_cr_tst_dir_/_cr_tst_dir_/_fn/lg1_extst/extst_1/ex2/_flow_tst.sh
+# echo -e "${HLIGHT}---start file://$filename ---${NORMAL}" # start file
+PW=$(pwd)
+idir="$(dirname ${filename})"
+cd "${idir}" || {
+    echo "${idir} not dir" >&2
+    return 1
+}
+#?----------------------------------------------------
+#?-------------------------------------
+
+: >res
+
+# cr_tst_dir_ <dat &>>res
+
+#{body_sh}
+#?-------------------------------------
+#?----------------------------------------------------
+cd "$idir"
+unset filename
+unset PW
+unset idir
+#{ish}
