5a0523a1b22a76067a1f5f318471492b2b552ec4
[alt]legioner9 <legioner9@inbox.ru>
2023-09-11 20:38:14 +0700

    <> add pycharm #2

--- Deploy_store/.qa/main_repo_fn/dir_mm_tsf_fsf_/mm_tsf_fsf_.sh
+++ Deploy_store/.qa/main_repo_fn/dir_mm_tsf_fsf_/mm_tsf_fsf_.sh
@@ -203,7 +203,7 @@ EXAMP:${NORMAL}"
     res+=("mm_tsf_fsf_define_files_dirs")
 
     local arr_name_flash=(TOSHIBA_EXT TOSH MY_ONE)
-    local arr_name_flow=(codium code pycharm_depricated)
+    local arr_name_flow=(codium code pycharm)
 
     mm_tsf_fsf_add_arrs() {
         for flow_item in "${arr_name_flow[@]}"; do
