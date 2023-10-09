5825fce6327482a7a6f9fcaa4892f3b00becb6c9
[alt]legioner9 <legioner9@inbox.ru>
2023-10-09 17:45:55 +0700

    <> ##

--- Deploy_store/.qa/lib/user/cd_e.sh
+++ Deploy_store/.qa/lib/user/cd_e.sh
@@ -2,7 +2,7 @@
 
 #. "$HOME/.bashrc"
 
-filename="${COMMUNIS_PATH}/Deploy_store/.qa/lib/user/untar.sh"
+filename="${COMMUNIS_PATH}/Deploy_store/.qa/lib/user/cd_e.sh"
 
 echo -e "${HLIGHT}---start file://$filename ---${NORMAL}" # start file
 
@@ -10,12 +10,12 @@ echo -e "${HLIGHT}---start file://$filename ---${NORMAL}" # start file
 
 #up_lib_git ${FUNCNAME[0]}
 
-untar() { #$1 string $2 delimer $3 num of sequence
+cd_e() { #$1 string $2 delimer $3 num of sequence
     # echo -e "${CYAN}--- ${FUNCNAME[0]}() $* ---${NORMAL}" #started functions
 
     # echo -e "${CYAN}--- ${FUNCNAME[0]}() $* ---${NORMAL}" #started functions
     local FNN=${FUNCNAME[0]}
-    local PPW=$(prs_f -d "${COMMUNIS_PATH}/Deploy_store/.qa/lib/user/untar.sh") # FILENAME==="filename"
+    local PPW=$(prs_f -d "${COMMUNIS_PATH}/Deploy_store/.qa/lib/user/cd_e.sh") # FILENAME==="filename"
     path_ext=$PPW/$FNN
 
     # amount_arg $# 1 1
@@ -53,29 +53,19 @@ untar() { #$1 string $2 delimer $3 num of sequence
         return 0
     fi
 
-    if [ -z "$ar2" ]; then
-        plt_err "\${ar2} not exist"
+    if [ -z "$ar1" ]; then
+        plt_err "\${ar1} not exist"
         return 1
     fi
 
-    # amount_arg $# min_args max_args
-    if [ "${ar1}" == "-t" ]; then
+    if [ -d "${ar1}" ]; then
 
-        plt_pause "do in ${PW} ? : tar czpvf - ${ar2}/ | split -d -b 25M - ${ar2}"
-        cd "${PW}" || plt_exit "NOT DIR :: ${PW}"
-        tar czpvf - ${ar2}/ | split -d -b 25M - ${ar2}
-        return 0
-    elif [ "${ar1}" == "-u" ]; then
-
-        plt_pause "do in ${PW} ?: cat ${ar2}* | tar xzpvf -"
-        cd "${PW}" || plt_exit "NOT DIR :: ${PW}"
-        cat "${ar2}"* | tar xzpvf -
+        cd "${ar1}"
         return 0
     else
-
-        plt_err "$2 not -t or -u"
-        return 1
+        plt_err "cd_e(): NOT DIR :: ${ar1}"
     fi
+
 }
 
 unset filename
